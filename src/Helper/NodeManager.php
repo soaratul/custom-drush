9106547a67b091122fb8cfbd1e4766696c264bd2
Atul Dubey <soaratul@gmail.com>
2017-05-27 14:34:20 +0530

    first commit

---
+++ src/Helper/NodeManager.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\mydrush\Helper;
+
+use Drupal\node\Entity\Node;
+
+Class NodeManager {
+
+  /**
+   * 
+   * @param type $data
+   */
+  public function createNode($data) {
+    if (!empty($data['title'])) {
+      $values = [
+        'type' => $data['type'],
+        'status' => 1,
+        'title' => $data['title'],
+        'body' => [
+          'value' => $data['body'],
+          'format' => 'full_html',
+        ],
+        'uid' => 1
+      ];
+      $node = Node::create($values);
+      $node->save();
+      if ($node->id()) {
+        return TRUE;
+      }
+    }
+    echo 555;
+    die;
+    return FALSE;
+  }
+
+}
